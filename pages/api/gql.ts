adf053f9c7649202e1d05e4a4c9634d5da4ad469
Kelly Davis <kelly.davis@forestry.io>
2023-06-22 17:00:44 -0400

    remove commented out

--- pages/api/gql.ts
+++ pages/api/gql.ts
@@ -1,19 +1,10 @@
 import { NextApiHandler } from "next";
-// import { isUserAuthorized } from "@tinacms/auth";
 import { databaseRequest } from "../../lib/databaseConnection";
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "./auth/[...nextauth]"
 
 const nextApiHandler: NextApiHandler = async (req, res) => {
-    const session = await getServerSession(req, res, authOptions)
-  // Use your own authentication logic here
-  // const isAuthorized = headers.authorization === "Bearer some-token"
-
-  // Example if using TinaCloud for auth
-  // const tinaCloudUser = await isUserAuthorized({
-  //   clientID: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  //   token: req.headers.authorization,
-  // });
+  const session = await getServerSession(req, res, authOptions)
 
   const isAuthorized =
     process.env.TINA_PUBLIC_IS_LOCAL === "true" ||
