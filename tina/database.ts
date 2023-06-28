e0c848f04cb047bd3c921d9e7c12a9f1f9f4592b
Kelly Davis <kelly.davis@forestry.io>
2023-06-28 16:25:23 -0400

    allow vercel env vars to be read

--- tina/database.ts
+++ tina/database.ts
@@ -1,27 +1,27 @@
-import { createDatabase, TinaLevelClient } from "@tinacms/datalayer";
-import { RedisLevel } from "upstash-redis-level";
-import { Octokit } from "@octokit/rest";
-import { Base64 } from "js-base64";
-import path from "path";
-import fs from "fs";
-import {Redis} from '@upstash/redis'
+import { createDatabase, TinaLevelClient } from '@tinacms/datalayer'
+import { Redis } from '@upstash/redis'
+import { RedisLevel } from 'upstash-redis-level'
+import { Octokit } from '@octokit/rest'
+import { Base64 } from 'js-base64'
+import path from 'path'
+import fs from 'fs'
 
 // Manage this flag in your CI/CD pipeline and make sure it is set to false in production
-const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
+const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === 'true'
 
-if (isLocal) console.log("Running TinaCMS in local mode.");
-else console.log("Running TinaCMS in production mode.");
+if (isLocal) console.log('Running TinaCMS in local mode.')
+else console.log('Running TinaCMS in production mode.')
 
-const owner = process.env.GITHUB_OWNER as string;
-const repo = process.env.GITHUB_REPO as string;
-const token = process.env.GITHUB_PERSONAL_ACCESS_TOKEN as string;
-const branch = process.env.GITHUB_BRANCH as string;
+const token = process.env.GITHUB_PERSONAL_ACCESS_TOKEN as string
+const owner = (process.env.GITHUB_OWNER || process.env.VERCEL_GIT_REPO_OWNER) as string
+const repo = (process.env.GITHUB_REPO || process.env.VERCEL_GIT_REPO_SLUG) as string
+const branch = (process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF) as string
 
 const octokit = new Octokit({
   auth: token,
-});
+})
 
-const localLevelStore = new TinaLevelClient();
+const localLevelStore = new TinaLevelClient()
 const redisLevelStore = new RedisLevel<string,Record<string,any>>({
   redis: new Redis({
     url: process.env.KV_REST_API_URL as string || 'http://localhost:8079',
@@ -29,10 +29,10 @@ const redisLevelStore = new RedisLevel<string,Record<string,any>>({
   }),
   debug: process.env.DEBUG === 'true' || false,
 })
-if (isLocal) localLevelStore.openConnection();
+if (isLocal) localLevelStore.openConnection()
 
 const githubOnPut = async (key, value) => {
-  let sha;
+  let sha
   try {
     const {
       // @ts-ignore
@@ -42,27 +42,27 @@ const githubOnPut = async (key, value) => {
       repo,
       path: key,
       ref: branch,
-    });
-    sha = existingSha;
+    })
+    sha = existingSha
   } catch (e) {}
 
   await octokit.repos.createOrUpdateFileContents({
     owner,
     repo,
     path: key,
-    message: "commit from self-hosted tina",
+    message: 'commit from self-hosted tina',
     content: Base64.encode(value),
     branch,
     sha,
-  });
-};
+  })
+}
 const localOnPut = async (key, value) => {
-  const currentPath = path.join(process.cwd(), key);
-  fs.writeFileSync(currentPath, value);
-};
+  const currentPath = path.join(process.cwd(), key)
+  fs.writeFileSync(currentPath, value)
+}
 
 const githubOnDelete = async (key) => {
-  let sha;
+  let sha
   try {
     const {
       // @ts-ignore
@@ -72,8 +72,8 @@ const githubOnDelete = async (key) => {
       repo,
       path: key,
       ref: branch,
-    });
-    sha = existingSha;
+    })
+    sha = existingSha
   } catch (e) {}
 
   if (sha) {
@@ -81,21 +81,21 @@ const githubOnDelete = async (key) => {
       owner,
       repo,
       path: key,
-      message: "commit from self-hosted tina",
+      message: 'commit from self-hosted tina',
       branch,
       sha,
-    });
+    })
   } else {
-    throw new Error(`Could not find file ${key} in repo ${owner}/${repo}`);
+    throw new Error(`Could not find file ${key} in repo ${owner}/${repo}`)
   }
-};
+}
 const localOnDelete = async (key) => {
-  const currentPath = path.join(process.cwd(), key);
-  fs.rmSync(currentPath);
-};
+  const currentPath = path.join(process.cwd(), key)
+  fs.rmSync(currentPath)
+}
 
 export default createDatabase({
   level: isLocal ? localLevelStore : redisLevelStore,
   onPut: isLocal ? localOnPut : githubOnPut,
   onDelete: isLocal ? localOnDelete : githubOnDelete,
-});
+})
