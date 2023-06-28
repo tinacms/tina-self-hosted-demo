a4680be742592f2f9b5e13c8c0fbb2a6ced85da1
Kelly Davis <kelly.davis@forestry.io>
2023-06-28 14:55:21 -0400

    fix up the login page

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,19 +1,57 @@
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { getCsrfToken } from "next-auth/react"
+import tinaLogo from '../../public/tina-logo.svg'
+import Image from "next/image";
 
 export default function SignIn({ csrfToken }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <form method="post" action="/api/auth/callback/credentials">
       <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-      <label>
-        Username
-        <input name="username" type="text" />
-      </label>
-      <label>
-        Password
-        <input name="password" type="password" />
-      </label>
-      <button type="submit">Sign in</button>
+      <div
+        className="grid h-screen w-screen place-items-center bg-slate-800 px-4 text-sm font-medium"
+      >
+        <div className="flex flex-col items-center justify-center gap-4">
+          <Image src={tinaLogo} alt="TinaCMS Logo" height={40}/>
+        </div>
+
+        <div className="w-full max-w-sm rounded-lg bg-slate-700/30 shadow">
+          <form className="p-4 md:p-5 lg:p-6">
+            <div className="grid gap-y-3">
+              <input
+                name="username"
+                className="focus:border-purple-400 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-200 outline-none transition placeholder:text-slate-400"
+                placeholder="email@example.com"
+              />
+              <input
+                name="password"
+                className="focus:border-purple-400 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-200 outline-none transition placeholder:text-slate-400"
+                type="password"
+              />
+              <button
+                className="flex items-center justify-center gap-x-2 rounded-md border border-slate-600 bg-slate-700 py-3 px-4 text-slate-300 transition hover:text-purple-400"
+              >
+                <svg
+                  style={{
+                    color: "rgb(203, 213, 225)",
+                  }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  fill="currentColor"
+                  className="bi"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"
+                    fill="#cbd5e1"
+                  ></path>
+                </svg>
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </form>
   )
 }
