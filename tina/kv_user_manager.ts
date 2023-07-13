e39729e43dd467445b40204febf0f685ef829ccb
Logan Anderson <landerson2@upei.ca>
2023-07-13 15:56:29 -0400

    update script to use loop

--- tina/kv_user_manager.ts
+++ tina/kv_user_manager.ts
@@ -6,160 +6,236 @@ import fs from "fs";
 import inquirer from "inquirer";
 import { Redis } from "@upstash/redis";
 
+const CHOICES = {
+  ADD: "Add a user",
+  UPDATE: "Update a user's password",
+  DELETE: "Delete a user",
+  EXIT: "Exit",
+};
 
 async function hashPassword(password) {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 }
 
-(async () => {
-  await fs.promises.stat('.env').catch(_ => {
-    fs.copyFileSync('.env.example', '.env')
-  })
-  dotenv.config()
+const promptUserForToken = async () => {
+  console.log("Did not find KV store url and token in .env file.");
+  // Prompt user for KV store url and token
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "url",
+      message: `Enter your Vercel KV Database URL (see https://vercel.com/dashboard/stores):`,
+    },
+    {
+      type: "input",
+      name: "token",
+      message: `Enter your Vercel KV Database token:`,
+    },
+    {
+      type: "input",
+      name: "credentials_key",
+      message: `Enter your Credentials key (default: 'tinacms_users'):`,
+      default: "tinacms_users",
+    },
+  ]);
 
-  console.log(chalk.green('Welcome to TinaCMS!'))
+  if (!process.env.KV_REST_API_URL) {
+    fs.writeFileSync(".env", `KV_REST_API_URL=${answers.url}\n`, { flag: "a" });
+    process.env.KV_REST_API_URL = answers.url;
+  }
+  if (!process.env.KV_REST_API_TOKEN) {
+    fs.writeFileSync(".env", `KV_REST_API_TOKEN=${answers.token}\n`, {
+      flag: "a",
+    });
+    process.env.KV_REST_API_TOKEN = answers.token;
+  }
+  if (!process.env.NEXTAUTH_CREDENTIALS_KEY) {
+    fs.writeFileSync(
+      ".env",
+      `NEXTAUTH_CREDENTIALS_KEY=${answers.credentials_key}\n`,
+      { flag: "a" }
+    );
+    process.env.NEXTAUTH_CREDENTIALS_KEY = answers.credentials_key;
+  }
+};
+
+const addUser = async (kv, name, password, passwordConfirm, users) => {
+  if (password !== passwordConfirm) {
+    console.log(chalk.red("Passwords do not match!"));
+    process.exit(1);
+  }
+  const user = {
+    name,
+    username: name,
+    password: await hashPassword(password),
+  };
+  if (!users) {
+    await kv.json.set(process.env.NEXTAUTH_CREDENTIALS_KEY, "$", {});
+  }
+  const res = await kv.json.set(
+    process.env.NEXTAUTH_CREDENTIALS_KEY,
+    `$.["${name}"]`,
+    user
+  );
+  if (res === "OK") {
+    console.log(chalk.green(`User ${name} added!`));
+  } else {
+    console.log(chalk.red(`Error adding user ${name}!`));
+  }
+};
+
+export const updatePassword = async (kv, name, password, passwordConfirm) => {
+  if (password !== passwordConfirm) {
+    console.log(chalk.red("Passwords do not match!"));
+    process.exit(1);
+  }
+  const user = await kv.json.get(
+    process.env.NEXTAUTH_CREDENTIALS_KEY,
+    `$.["${name}"]`
+  );
+  if (!user) {
+    console.log(chalk.red(`User ${name} not found!`));
+    process.exit(1);
+  }
+  const hash = await hashPassword(password);
+  const res = await kv.json.set(
+    process.env.NEXTAUTH_CREDENTIALS_KEY,
+    `$.["${name}"].password`,
+    `"${hash}"`
+  );
+  if (res === "OK") {
+    console.log(chalk.green(`User ${name} password updated!`));
+  } else {
+    console.log(chalk.red(`Error updating user ${name} password!`));
+  }
+};
+
+export const deleteUser = async (kv, name) => {
+  const user = await kv.json.get(
+    process.env.NEXTAUTH_CREDENTIALS_KEY,
+    `$.["${name}"]`
+  );
+  if (!user) {
+    console.log(chalk.red(`User ${name} not found!`));
+    process.exit(1);
+  }
+  const res = await kv.json.del(
+    process.env.NEXTAUTH_CREDENTIALS_KEY,
+    `$.["${name}"]`
+  );
+  if (res === 1) {
+    console.log(chalk.green(`User ${name} deleted!`));
+  } else {
+    console.log(chalk.red(`Error deleting user ${name}!`));
+  }
+};
+
+(async () => {
+  await fs.promises.stat(".env").catch((_) => {
+    fs.copyFileSync(".env.example", ".env");
+  });
+  dotenv.config();
+
+  console.log(chalk.green("Welcome to TinaCMS!"));
 
   if (!process.env.KV_REST_API_URL || !process.env.KV_REST_API_TOKEN) {
-    // Prompt user for KV store url and token
-    const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'url',
-        message: `Enter your Vercel KV Database URL (see https://vercel.com/dashboard/stores):`,
-      },
-      {
-        type: 'input',
-        name: 'token',
-        message: `Enter your Vercel KV Database token:`,
-      },
-      {
-        type: 'input',
-        name: 'credentials_key',
-        message: `Enter your Credentials key (default: 'tinacms_users'):`,
-        default: 'tinacms_users',
-      }
-    ])
-
-    if (!process.env.KV_REST_API_URL) {
-      fs.writeFileSync('.env', `KV_REST_API_URL=${answers.url}\n`, { flag: 'a' })
-      process.env.KV_REST_API_URL = answers.url
-    }
-    if (!process.env.KV_REST_API_TOKEN) {
-      fs.writeFileSync('.env', `KV_REST_API_TOKEN=${answers.token}\n`, { flag: 'a' })
-      process.env.KV_REST_API_TOKEN = answers.token
-    }
-    if (!process.env.NEXTAUTH_CREDENTIALS_KEY) {
-      fs.writeFileSync('.env', `NEXTAUTH_CREDENTIALS_KEY=${answers.credentials_key}\n`, { flag: 'a' })
-      process.env.NEXTAUTH_CREDENTIALS_KEY = answers.credentials_key
-    }
+    await promptUserForToken();
   }
 
+  let done = false;
   const kv = new Redis({
     url: process.env.KV_REST_API_URL,
     token: process.env.KV_REST_API_TOKEN,
-  })
+  });
 
-  const users = await kv.json.get(process.env.NEXTAUTH_CREDENTIALS_KEY)
-  if (!users || Object.keys(users).length === 0) {
-    console.log(chalk.red('No users found!'))
-  } else {
-    console.log(chalk.green('Users:'))
-    console.table(Object.keys(users).map(username => {
-      return {
-        name: username,
+  // Loop until user exits
+  while (!done) {
+    let users = null;
+    try {
+      users = await kv.json.get(process.env.NEXTAUTH_CREDENTIALS_KEY);
+    } catch (e) {} // eslint-disable-line
+
+    if (!users || Object.keys(users).length === 0) {
+      console.log(chalk.red("No users found! Please add a user."));
+    } else {
+      console.log(chalk.green("Users:"));
+      console.table(
+        Object.keys(users).map((username) => {
+          return {
+            name: username,
+          };
+        })
+      );
+    }
+
+    const answers = await inquirer.prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: `What would you like to do?`,
+        choices: [CHOICES.ADD, CHOICES.UPDATE, CHOICES.DELETE, CHOICES.EXIT],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: `Enter a username:`,
+        when: (answers) =>
+          answers.choice === CHOICES.ADD ||
+          answers.choice === CHOICES.UPDATE ||
+          answers.choice === CHOICES.DELETE,
+      },
+      {
+        type: "password",
+        name: "password",
+        message: `Enter a user password:`,
+        when: (answers) =>
+          answers.choice === CHOICES.ADD || answers.choice === CHOICES.UPDATE,
+      },
+      {
+        type: "password",
+        name: "passwordConfirm",
+        message: `Confirm the user password:`,
+        when: (answers) =>
+          answers.choice === CHOICES.ADD || answers.choice === CHOICES.UPDATE,
+      },
+    ]);
+
+    if (answers.choice === CHOICES.ADD) {
+      const { name, password, passwordConfirm } = answers;
+      try {
+        await addUser(kv, name, password, passwordConfirm, users);
+      } catch (e) {
+        console.log(
+          chalk.red(
+            `An unexpected error occurred while adding a user. ${e.message}}`
+          )
+        );
       }
-    }))
-  }
-
-  const answers = await inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'addUser',
-      message: `Would you like to add a new user?`,
-    },
-    {
-      type: 'confirm',
-      name: 'updatePassword',
-      message: `Would you like to update a user's password?`,
-      when: (answers) => !answers.addUser,
-    },
-    {
-      type: 'confirm',
-      name: 'deleteUser',
-      message: `Would you like to delete a user?`,
-      when: (answers) => !answers.addUser && !answers.updatePassword,
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: `Enter a username:`,
-      when: (answers) => answers.addUser || answers.updatePassword || answers.deleteUser,
-    },
-    {
-      type: 'password',
-      name: 'password',
-      message: `Enter a user password:`,
-      when: (answers) => answers.addUser || answers.updatePassword,
-    },
-    {
-      type: 'password',
-      name: 'passwordConfirm',
-      message: `Confirm the user password:`,
-      when: (answers) => answers.addUser || answers.updatePassword,
-    }
-    ])
-
-  if (answers.addUser) {
-    const { name, password, passwordConfirm } = answers
-    if (password !== passwordConfirm) {
-      console.log(chalk.red('Passwords do not match!'))
-      process.exit(1)
-    }
-    const user = {
-      name,
-      username: name,
-      password: await hashPassword(password),
-    }
-    if (!users) {
-      await kv.json.set(process.env.NEXTAUTH_CREDENTIALS_KEY, '$', {})
-    }
-    const res = await kv.json.set(process.env.NEXTAUTH_CREDENTIALS_KEY, `$.${name}`, user)
-    if (res === 'OK') {
-      console.log(chalk.green(`User ${name} added!`))
-    } else {
-      console.log(chalk.red(`Error adding user ${name}!`))
-    }
-  } else if (answers.updatePassword) {
-    const { name, password, passwordConfirm } = answers
-    if (password !== passwordConfirm) {
-      console.log(chalk.red('Passwords do not match!'))
-      process.exit(1)
-    }
-    const user = await kv.json.get(process.env.NEXTAUTH_CREDENTIALS_KEY, `$.${name}`)
-    if (!user) {
-      console.log(chalk.red(`User ${name} not found!`))
-      process.exit(1)
-    }
-    const hash = await hashPassword(password)
-    const res = await kv.json.set(process.env.NEXTAUTH_CREDENTIALS_KEY, `$.${name}.password`, `"${hash}"`)
-    if (res === 'OK') {
-      console.log(chalk.green(`User ${name} password updated!`))
-    } else {
-      console.log(chalk.red(`Error updating user ${name} password!`))
-    }
-  } else if (answers.deleteUser) {
-    const { name } = answers
-    const user = await kv.json.get(process.env.NEXTAUTH_CREDENTIALS_KEY, `$.${name}`)
-    if (!user) {
-      console.log(chalk.red(`User ${name} not found!`))
-      process.exit(1)
-    }
-    const res = await kv.json.del(process.env.NEXTAUTH_CREDENTIALS_KEY, `$.${name}`)
-    if (res === 1) {
-      console.log(chalk.green(`User ${name} deleted!`))
-    } else {
-      console.log(chalk.red(`Error deleting user ${name}!`))
+    } else if (answers.choice === CHOICES.UPDATE) {
+      const { name, password, passwordConfirm } = answers;
+      try {
+        await updatePassword(kv, name, password, passwordConfirm);
+      } catch (e) {
+        console.log(
+          chalk.red(
+            `An unexpected error occurred while adding a updating the password for ${name}. ${e.message}}`
+          )
+        );
+      }
+    } else if (answers.choice === CHOICES.DELETE) {
+      const { name } = answers;
+      try {
+        await deleteUser(kv, name);
+      } catch (e) {
+        console.log(
+          chalk.red(
+            `An unexpected error occurred while deleting the user ${name}. ${e.message}}`
+          )
+        );
+      }
+    } else if (answers.choice === CHOICES.EXIT) {
+      done = true;
     }
   }
-})()
+})();
