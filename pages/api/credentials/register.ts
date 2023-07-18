7fb0b841f489e6cdc0a7c0d7803f88b039b565da
Kelly Davis <kelly.davis@forestry.io>
2023-07-18 15:27:35 -0400

    debug user creation

--- pages/api/credentials/register.ts
+++ pages/api/credentials/register.ts
@@ -1,12 +1,25 @@
 import { userStore } from "../../../tina/nextauth";
 
 export default function handler(req, res) {
+  console.log('register handler')
   const { username, password } = req.body
+  console.log({username, password})
   if (req.method === 'POST') {
     if (!username || !password) {
       res.status(400).json({ message: 'Missing username or password' })
     } else {
-      return userStore.addUser(username, password)
+      console.log('calling addUser')
+      try {
+        const success = userStore.addUser(username, password)
+        if (success) {
+          res.status(200).json({ message: 'User added' })
+        } else {
+          res.status(400).json({ message: 'User already exists' })
+        }
+      } catch (e) {
+        console.log(e)
+        res.status(500).json({ message: 'Internal server error' })
+      }
     }
   } else {
     res.status(400).json({ message: 'Invalid request' })
