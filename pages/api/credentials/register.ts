e18feb5e699b3e3b0dbfe991fcc01ed07c53fb46
Kelly Davis <kelly.davis@forestry.io>
2023-07-18 16:12:10 -0400

    remove console debugs

--- pages/api/credentials/register.ts
+++ pages/api/credentials/register.ts
@@ -1,14 +1,11 @@
 import { userStore } from "../../../tina/nextauth";
 
 export default async function handler(req, res) {
-  console.log('register handler')
   const { username, password } = req.body
-  console.log({username, password})
   if (req.method === 'POST') {
     if (!username || !password) {
       res.status(400).json({ message: 'Missing username or password' })
     } else {
-      console.log('calling addUser')
       try {
         const success = await userStore.addUser(username, password)
         if (success) {
