5c96724df4b0b183a574f3ee1869d5d6b2f941ba
James O'Halloran <james.p.ohalloran@gmail.com>
2023-08-23 15:07:25 -0300

    Bump versions

--- pages/api/credentials/register.ts
+++ pages/api/credentials/register.ts
@@ -1,38 +1,25 @@
-import {Redis} from '@upstash/redis'
-import bcrypt from 'bcryptjs'
+// THIS FILE HAS BEEN GENERATED WITH THE TINA CLI.
+import { userStore } from "../../../tina/auth";
 
-async function hashPassword(password) {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
-}
-
-async function createUser(username, password, res) {
-  const kv = new Redis({
-    url: process.env.KV_REST_API_URL,
-    token: process.env.KV_REST_API_TOKEN,
-  })
-  const users = await kv.json.get(process.env.NEXTAUTH_CREDENTIALS_KEY)
-  if (!users || Object.keys(users).length === 0) {
-    const output = await kv.json.set(process.env.NEXTAUTH_CREDENTIALS_KEY, '$', { [username]: { name: username, username, password: await hashPassword(password) } })
-    if (output === 'OK') {
-      res.status(200).json({ message: 'User added' })
-    } else {
-      res.status(500).json({ message: 'Error adding user' })
-    }
-  } else {
-    res.status(400).json({ message: 'User setup already completed' })
-  }
-}
-
-export default function handler(req, res) {
-  const { username, password } = req.body
-  if (req.method === 'POST') {
+export default async function handler(req, res) {
+  const { username, password } = req.body;
+  if (req.method === "POST") {
     if (!username || !password) {
-      res.status(400).json({ message: 'Missing username or password' })
+      res.status(400).json({ message: "Missing username or password" });
     } else {
-      return createUser(username, password, res)
+      try {
+        const success = await userStore.addUser(username, password);
+        if (success) {
+          res.status(200).json({ message: "User added" });
+        } else {
+          res.status(400).json({ message: "User already exists" });
+        }
+      } catch (e) {
+        console.error(e);
+        res.status(500).json({ message: "Internal server error" });
+      }
     }
   } else {
-    res.status(400).json({ message: 'Invalid request' })
+    res.status(400).json({ message: "Invalid request" });
   }
 }
