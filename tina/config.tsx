99f4f1570092ad6ffc09191a60a74606f1a858be
Logan Anderson <landerson2@upei.ca>
2023-11-17 13:07:25 -0500

    move page collection to its own file

--- tina/config.tsx
+++ tina/config.tsx
@@ -3,7 +3,11 @@ import {
   TinaUserCollection,
 } from "tinacms-authjs/dist/tinacms";
 import { defineConfig, LocalAuthProvider } from "tinacms";
+
+import { PageCollection } from "./collections/page";
+
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
+
 export default defineConfig({
   authProvider: isLocal
     ? new LocalAuthProvider()
@@ -21,51 +25,6 @@ export default defineConfig({
     },
   },
   schema: {
-    collections: [
-      TinaUserCollection,
-      {
-        name: "page",
-        label: "Page",
-        path: "content/pages",
-        format: "md",
-        ui: {
-          router: () => "/",
-        },
-        fields: [
-          {
-            type: "string",
-            name: "header",
-            label: "Header",
-          },
-          {
-            type: "object",
-            name: "logo",
-            label: "Logo",
-            fields: [
-              { type: "image", name: "url", label: "URL" },
-              { type: "string", name: "alt", label: "Alt Text" },
-            ],
-          },
-          {
-            type: "object",
-            list: true,
-            name: "links",
-            label: "Links",
-            ui: {
-              itemProps: (item) => {
-                return {
-                  label: item?.header,
-                };
-              },
-            },
-            fields: [
-              { type: "string", name: "header" },
-              { type: "string", name: "description" },
-              { type: "string", name: "url" },
-            ],
-          },
-        ],
-      },
-    ],
+    collections: [TinaUserCollection, PageCollection],
   },
 });
