03d945c844f6cda1b3d3086c4cb91fd545294728
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2024-07-11 14:54:55 -0700

    Text size update

--- components/features/product/index.tsx
+++ components/features/product/index.tsx
@@ -9,9 +9,12 @@ export default function Product(props: ProductQuery['product']) {
         <div className='p-12 h-full flex flex-col gap-8 bg-gray-100 rounded-md '>
             <Image src={productImages?.[0] || ''} height={600} width={400} alt={title || ''} className='m-auto transition scale-1 hover:scale-105 duration-200 ease-in-out flex-1' />
             <div className=' flex-1 flex flex-col gap-8 justify-between ' >
-                <h2 className='text-xl sm:text-2xl font-bold tracking-wide'>{title}</h2>
-                <TinaMarkdown content={overviewDescription} />
-                <Link href={`/products/${uniqueId}`} className='rounded-full w-fit px-6 py-2 transition duration-700 ease-in-out border-brandSecondary text-brandSecondary font-bold text-center text-sm tracking-wide border-2  hover:bg-transparent hover:text-black  '>
+                <h2 className='text-4xl font-semibold tracking-wide'>{title}</h2>
+                <div className='text-gray-600'>
+                    <TinaMarkdown content={overviewDescription} />
+                </div>
+
+                <Link href={`/products/${uniqueId}`} className='rounded-full shadow w-fit px-6 py-2.5 transition duration-300 ease-in-out border-brandSecondary font-semibold text-center text-sm border-solid border-2  hover:bg-transparent hover:scale-110  '>
                     Discover The Product
                 </Link>
             </div>
