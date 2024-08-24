816ac0b28599baaac492381d1788ebfa79936d3f
Kelly Davis <kldavis4@gmail.com>
2024-08-24 13:01:08 -0400

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -20,4 +24,4 @@ module.exports = {
       },
     ];
   },
-};
+});
