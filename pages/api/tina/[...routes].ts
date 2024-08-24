5e855b106c72b24401970540ccb64206abee2df7
Kelly Davis <kldavis4@gmail.com>
2024-08-24 12:35:06 -0400

    update

--- pages/api/tina/[...routes].ts
+++ pages/api/tina/[...routes].ts
@@ -1,5 +1,5 @@
 import { TinaAuthJSOptions, AuthJsBackendAuthProvider } from "tinacms-authjs";
-import { TinaEdgeBackend } from "@tinacms/datalayer";
+import { TinaEdgeBackend } from "@tinacms/datalayer/dist/index-edge";
 
 import databaseClient from "../../../tina/__generated__/databaseClient";
 
@@ -9,12 +9,16 @@ const handler = TinaEdgeBackend({
           databaseClient: databaseClient,
           secret: process.env.NEXTAUTH_SECRET!,
         }),
-      }),
+      }) as any,
   databaseClient,
 });
 
 export const config = {
-  runtime: 'edge'
+  runtime: 'edge',
+  unstable_allowDynamic: [
+    './pages/api/tina/[...routes].ts',
+    '/node_modules/**',
+  ],
 };
 
 export default async (req: Request): Promise<Response> => {
